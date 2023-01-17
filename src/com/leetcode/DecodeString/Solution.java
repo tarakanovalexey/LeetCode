4a848df39f0efc6a82c4040eaa1fc16467490dd6
Алексей Тараканов <ADmTarakanov@sberbank.ru>
2023-01-17 20:05:28 +0300

    Decode String

--- src/com/leetcode/DecodeString/Solution.java
+++ src/com/leetcode/DecodeString/Solution.java
@@ -6,16 +6,17 @@ public class Solution {
     The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being
     repeated exactly k times. Note that k is guaranteed to be a positive integer.
 
-    You may assume that the input string is always valid; there are no extra white spaces, square brackets
-    are well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and
-    that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
+    You may assume that the input string is always valid; there are no extra white spaces, square brackets are
+    well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and that
+    digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
 
-    The test cases are generated so that the length of the output will never exceed 10^5.*/
+    The test cases are generated so that the length of the output will never exceed 105.*/
 
     public static void main(String[] args) {
         assertion(decodeString("3[a]2[bc]"), "aaabcbc");
         assertion(decodeString("3[a2[c]]"), "accaccacc");
         assertion(decodeString("2[abc]3[cd]ef"), "abcabccdcdcdef");
+        assertion(decodeString("2[ab3[cd]]4[xy]"), "abcdcdcdabcdcdcdxyxyxyxy");
     }
 
     public static String decodeString(String s) {
@@ -29,14 +30,37 @@ public class Solution {
                 mult.append(s.charAt(i));
             } else if (s.charAt(i) == 91) {
                 counter++;
-            } else if (s.charAt(i) == 93) {
                 if (counter == 1) {
                     multi = Integer.parseInt(mult.toString());
-                    for (int j = 0; j < multi; j++) {
-                        result.append(decodeString(seq.toString()));
+                    StringBuilder underseq = new StringBuilder();
+                    int c = 0;
+                    for (int o = i+1; o < s.length(); o++) {
+                        if (s.charAt(o) == 91) {
+                            underseq.append(s.charAt(o));
+                            c++;
+                        } else if (s.charAt(o) == 93 && c == 0) {
+                            break;
+                        } else if (s.charAt(o) == 93) {
+                            underseq.append(s.charAt(o));
+                            c--;
+                        } else {
+                            underseq.append(s.charAt(o));
+                        }
                     }
+                    for (int j = 0; j < multi; j++) {
+                        result.append(decodeString(underseq.toString()));
+                    }
+                    mult = new StringBuilder();
+                    seq = new StringBuilder();
                 }
+            } else if (s.charAt(i) == 93) {
                 counter--;
+                if (counter == 0) {
+                    mult = new StringBuilder();
+                    seq = new StringBuilder();
+                }
+            } else if (counter == 0) {
+                result.append(s.charAt(i));
             } else {
                 seq.append(s.charAt(i));
             }
